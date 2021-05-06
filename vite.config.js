961e159b85f584c104a550e47c3f36c9e654b61c
dirkliu <liuqi0702@126.com>
2021-05-06 16:50:49 +0800

    px2vw plugin updated!

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vitePluginImp from 'vite-plugin-imp'
-import px2vw from './rollup-plugin-px2vw'
+import px2vw from 'rollup-plugin-px2vw'
 import { resolve } from 'path'
 
 // https://vitejs.dev/config/
