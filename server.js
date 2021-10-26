d65f1d5b28140a1777cecd8b8868ce583d0604d1
dirkliu <liuqi0702@126.com>
2021-10-26 21:04:30 +0800

    change server!

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const {
+  scryptSync,
+  createCipheriv,
   createHash
 } = require('crypto')
 
-const hash = createHash('md5');
+const hash = createHash('md5')
+const password = '7a1111'
 
-hash.update('test');
-console.log(hash.digest('hex'));
+hash.update(password)
+
+const key1 = scryptSync(password, '', 16)
+console.log(key1.toString('hex'))  // '3745e48...08d59ae'
+console.log(hash.digest('hex'))
