5265f7fc9bdfcd5aa6ea4e5c19af4062da7b2020
dirkliu <liuqi0702@126.com>
2021-11-03 00:00:14 +0800

    bufix:气泡优化！

--- src/pop/PopApp.vue
+++ src/pop/PopApp.vue
@@ -1,6 +1,24 @@
 <template>
   <div class="app-wrapper">
-    <qi-pop></qi-pop>
+    <qi-pop>
+      <div class="text-block">
+        asdf fdafdsa  fasfas 
+        fasfas fasfas
+        <div>afasf<a>我是里面啊</a></div>
+      </div>
+      <template #content>
+        <div>气泡内容</div>
+      </template>
+    </qi-pop>
+
+    <p>
+      这里的 lovingVue 的值将会传入这个名为 checked 的 prop。同时当 触发一个 change 事件并附带一个新的值的时候,这个 lovingVue 的 property 将会被更新
+      <qi-pop>
+        <a>点我有气泡</a>
+
+        <template #content>我是一个气泡</template>
+      </qi-pop>
+    </p>
   </div>
 </template>
 
@@ -18,4 +36,7 @@ export default defineComponent({
 </script>
 
 <style lang="scss">
+.text-block {
+  background-color: aquamarine;
+}
 </style>
--- src/pop/components/QiPop.vue
+++ src/pop/components/QiPop.vue
@@ -1,28 +1,54 @@
 <template>
   <div class="qi-pop-wrapper">
     <teleport to="body">
-      <div class="pop-container"></div>
+      <div class="pop-container" v-if="visible">
+        <slot name="content"></slot>     
+      </div>
     </teleport>
+
+    <div class="trigger" @click.stop="onToggle">
+      <slot></slot>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 export default defineComponent({
+  data () {
+    return {
+      visible: false
+    }
+  },
+
   methods: {
     getZIndex (el: HTMLElement) {
       document.getElementById('#od')
       window.getComputedStyle(el,null).getPropertyValue('z-index');
+    },
+
+    onToggle () {
+      console.log('onToggle:', this.visible)
+      this.visible = !this.visible
     }
-  } 
+  },
+
+  mounted () {
+    window.addEventListener('click', e => {
+      console.log('window click')
+      this.visible = false
+    })
+  }
 })
 </script>
 
 <style lang="scss" scoped>
 .pop-container {
-  padding: 10px;
-  min-width:50px;
+  padding: 5px;
+  min-width:20px;
   min-height: 20px;
+  max-width: 100%;
+  box-sizing: border-box;
   position: fixed;
   top: 20px;
   left: 20px;
