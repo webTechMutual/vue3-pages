e4322a28c5739234d8d88b7af4b55682fd13d948
dirkliu <liuqi0702@126.com>
2021-11-21 12:58:35 +0800

    bugfix!

--- src/index/main.ts
+++ src/index/main.ts
@@ -3,10 +3,8 @@ import { createApp } from 'vue'
 import router from "./router/index"
 import App from './AppIndex.vue'
 import req from '../commons/req'
-import tipTop from '../plugins/tipTop'
 
 const app = createApp(App)
-app.use(tipTop)
 app.use(router)
 app.use(req)
 app.mount('#app')
